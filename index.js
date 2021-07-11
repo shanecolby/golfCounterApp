bd600fb475e7ddfcbdb57f0900f2b20cf3e202a7
shanecolby <shanecolbyzero8@gmail.com>
2021-07-11 11:24:37 -0500

    finished basic counter, adding total score

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
+let scoreEl = document.getElementById("score-el")
 let count = 0
+let scores = []
+
 
 function increment() {
   count += 1
@@ -8,5 +11,13 @@ function increment() {
 }
 
 function save() {
-  console.log("clciked")
+  scores.push(count)
+  let countString = count + " - "
+  saveEl.textContent += countString
+  countEl.textContent = 0
+  count = 0
 }
+
+
+console.log(scores)
+
