a5a2d42ff704c7ca76f1790d54b331035d6d4bb0
shanecolby <shanecolbyzero8@gmail.com>
2021-07-11 10:58:42 -0500

    create increment stroke function to display hole total by number of clicks

---
+++ index.js
@@ -0,0 +1,12 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+  count += 1
+  countEl.textContent = count
+}
+
+function save() {
+  console.log("clciked")
+}
