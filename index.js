d12dc2c6fb4ca9d68341c0d0ee71d7e3796e2b27
shanecolby <shanecolbyzero8@gmail.com>
2021-07-11 19:56:48 -0500

    style tally button

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
 let scoreEl = document.getElementById("score-el")
+let cPlusThree = document.getElementById("countPlusThree-btn")
 let count = 0
 let scores = []
+
 
 
 function increment() {
@@ -17,6 +19,7 @@ function save() {
   countEl.textContent = 0
   count = 0
 }
+
 
 
 function total() {
